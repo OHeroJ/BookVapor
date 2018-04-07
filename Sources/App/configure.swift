ccae63d5db4f65c1bdf3eeb755ec7c6421884dee
oheroj <1164258202@qq.com>
2018-04-07 15:52:42 +0800

    'created Hello from template https://github.com/vapor/api-template'

---
+++ Sources/App/configure.swift
@@ -0,0 +1,47 @@
+import FluentSQLite
+import Vapor
+
+/// Called before your application initializes.
+///
+/// https://docs.vapor.codes/3.0/getting-started/structure/#configureswift
+public func configure(
+    _ config: inout Config,
+    _ env: inout Environment,
+    _ services: inout Services
+) throws {
+    /// Register providers first
+    try services.register(FluentSQLiteProvider())
+
+    /// Register routes to the router
+    let router = EngineRouter.default()
+    try routes(router)
+    services.register(router, as: Router.self)
+
+    /// Register middleware
+    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
+    /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+    middlewares.use(DateMiddleware.self) // Adds `Date` header to responses
+    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+    services.register(middlewares)
+
+    // Configure a SQLite database
+    let sqlite: SQLiteDatabase
+    if env.isRelease {
+        /// Create file-based SQLite db using $SQLITE_PATH from process env
+        sqlite = try SQLiteDatabase(storage: .file(path: Environment.get("SQLITE_PATH")!))
+    } else {
+        /// Create an in-memory SQLite database
+        sqlite = try SQLiteDatabase(storage: .memory)
+    }
+
+    /// Register the configured SQLite database to the database config.
+    var databases = DatabaseConfig()
+    databases.add(database: sqlite, as: .sqlite)
+    services.register(databases)
+
+    /// Configure migrations
+    var migrations = MigrationConfig()
+    migrations.add(model: Todo.self, database: .sqlite)
+    services.register(migrations)
+
+}
---
+++ Sources/App/routes.swift
@@ -0,0 +1,18 @@
+import Routing
+import Vapor
+
+/// Register your application's routes here.
+///
+/// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
+public func routes(_ router: Router) throws {
+    // Basic "Hello, world!" example
+    router.get("hello") { req in
+        return "Hello, world!"
+    }
+
+    // Example of configuring a controller
+    let todoController = TodoController()
+    router.get("todos", use: todoController.index)
+    router.post("todos", use: todoController.create)
+    router.delete("todos", Todo.parameter, use: todoController.delete)
+}
