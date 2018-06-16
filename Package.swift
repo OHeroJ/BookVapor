eb374294159aa69ec38f544c09261c926e99ef17
oheroj <1164258202@qq.com>
2018-06-16 10:52:16 +0800

    Feat: 去除重复依赖

--- Package.swift
+++ Package.swift
@@ -8,11 +8,10 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc.3"),
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/crypto.git", from: "3.2.0"),
-        .package(url: "https://github.com/vapor/console.git", from: "3.0.2")
+
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentMySQL", "Vapor", "SwiftyJSON", "Authentication", "Crypto", "Logging"]),
+        .target(name: "App", dependencies: ["FluentMySQL", "Vapor", "SwiftyJSON", "Authentication"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
