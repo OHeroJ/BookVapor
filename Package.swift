d392279fbef202991025d4c53b6e1e48f9d5ec56
oheroj <1164258202@qq.com>
2018-08-10 17:23:19 +0800

    Feat: 包升级

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@ import PackageDescription
 let package = Package(
     name: "Hello",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.4"),
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc"),
-        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.8"),
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
         .package(url: "https://github.com/vapor-community/pagination.git", from: "1.0.6"),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.1.0"),
         .package(url: "https://github.com/vapor-community/sendgrid-provider.git", from: "3.0.5"),
-        .package(url: "https://github.com/skelpo/APIErrorMiddleware.git", from: "0.3.3")
+        .package(url: "https://github.com/skelpo/APIErrorMiddleware.git", from: "0.3.5")
 
     ],
     targets: [
