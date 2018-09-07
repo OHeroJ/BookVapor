8788734c8a9d4fb5793ac7aaf3cbd464e5e287fa
oheroj <1164258202@qq.com>
2018-09-07 11:55:14 +0800

    Fix: api 不兼容

--- Sources/App/Utils/Ext/String+Ext.swift
+++ Sources/App/Utils/Ext/String+Ext.swift
@@ -13,9 +13,14 @@ extension String {
         var randomString: String = ""
 
         for _ in 0..<length {
+            #if os(Linux)
+            let randomValue = Int(random() % UInt32(base.count))
+            #else
             let randomValue = arc4random_uniform(UInt32(base.count))
+            #endif
             randomString += "\(base[base.index(base.startIndex, offsetBy: Int(randomValue))])"
         }
         return randomString
     }
 }
+
